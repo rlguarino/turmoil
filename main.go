d322f9f64bebb5276420d4f2d221c72e7b687cf5
Ross Guarino <dev@rlg.io>
2024-08-19 10:39:17 -0700

    ability to run more than one node and create a "latest" symlink to most recent run

--- main.go
+++ main.go
@@ -6,70 +6,119 @@ import (
 	"log/slog"
 	"os"
 	"os/exec"
+	"path/filepath"
 	"sync"
 	"time"
 )
 
-func testTempDir(t time.Time) string {
-	return fmt.Sprintf("store/%s", t.Format("20060102150405.000"))
+type Store string
+
+// SetupStore will create a store for use in a specific run.
+// We will create all of the dictories and update the "latest" symlink to point at the latest store
+func SetupStore(now time.Time) Store {
+	base := "store"
+	storeDir := filepath.Join(base, now.Format("20060102150405.000"))
+	err := os.MkdirAll(storeDir, 0755)
+	if err != nil {
+		panic(err)
+	}
+	latestSymlink := filepath.Join(base, "latest")
+	os.Remove(latestSymlink)
+	err = os.Symlink(storeDir, latestSymlink)
+	if err != nil {
+		panic(err)
+	}
+	return Store(storeDir)
 }
 
-// TODO: Create a dir for run results, save stdERR to that file, save messages to that file, save some results to that directory as well?
-// TODO: Create a client that will send a specific QPS!
-// TODO: It would be cool to have a HTTP interface for results, then maybe the runs could be done on a K8s cluster?
-// TODO: I feel like it would be nicer to run docker containers, maybe way later.
-func main() {
-	now := time.Now()
-	tempDir := testTempDir(now)
-	// err := os.Mkdir("store", 0755)
-	// if err != nil {
-	// 	panic(err)
-	// }
-	err := os.Mkdir(tempDir, 0755)
+// createStoreFile creates or truncates a file and all parent the parent directories in the given store
+// if filename contains a direcotry then the directory will be created before the file with mode 0755
+func (s Store) createStoreFile(filename string) (*os.File, error) {
+	err := os.MkdirAll(filepath.Join(string(s), filepath.Dir(filename)), 0755)
 	if err != nil {
-		panic(err)
+		return nil, err
 	}
-	n1StderrFile, err := os.Create(fmt.Sprintf("%s/%s", tempDir, "n1.log"))
-	if err != nil {
-		panic(err)
-	}
+	return os.Create(filepath.Join(string(s), filename))
+}
 
-	serverExecPath := "./echo-server"
-	c := exec.Command(serverExecPath)
-	c.Stderr = n1StderrFile
-	serverStdout, err := c.StdoutPipe()
-	if err != nil {
-		panic(err)
-	}
-	serverStdin, err := c.StdinPipe()
-	if err != nil {
-		panic(err)
-	}
+type Network struct {
+	store          Store
+	nodeQueuesLock sync.Mutex
+	nodeQueues     map[string]chan string
+}
 
-	wg := sync.WaitGroup{}
-	wg.Add(1)
+func NewNetwork(s Store) *Network {
+	return &Network{
+		s,
+		sync.Mutex{},
+		make(map[string]chan string),
+	}
+}
+
+func (n *Network) StartNode(name string, executablePath string) error {
+	stderrFh, err := n.store.createStoreFile(filepath.Join("node_logs", fmt.Sprintf("%s.log", name)))
+	if err != nil {
+		return fmt.Errorf("error creating node log file: %w", err)
+	}
+	c := exec.Command(executablePath)
+	c.Stderr = stderrFh
+	nodeStdout, err := c.StdoutPipe()
+	if err != nil {
+		return fmt.Errorf("error creating StdoutPipe: %w", err)
+	}
+	nodeStdin, err := c.StdinPipe()
+	if err != nil {
+		return fmt.Errorf("error creating StdinPipe: %w", err)
+	}
+	inbox := make(chan string)
+	n.nodeQueuesLock.Lock()
+	if _, ok := n.nodeQueues[name]; ok {
+		panic(fmt.Sprintf("node already exists with name:%v", name))
+	}
+	n.nodeQueues[name] = inbox
+	n.nodeQueuesLock.Unlock()
 	go func() {
-		defer wg.Done()
-		stdout := bufio.NewScanner(serverStdout)
+		stdout := bufio.NewScanner(nodeStdout)
 		for stdout.Scan() {
 			line := stdout.Text()
+			// TODO: Queue the message in the destinations channel
 			slog.Info("stdout", "line", line)
 		}
 		slog.Info("finished reading from stdout")
 	}()
-	wg.Add(1)
+
 	go func() {
-		defer wg.Done()
-		writer := bufio.NewWriter(serverStdin)
+		writer := bufio.NewWriter(nodeStdin)
 		writer.WriteString("{\"type\":\"init\"}\n")
 		err := writer.Flush()
 		if err != nil {
 			slog.Error("error flushing", "command", c, "error", err)
 		}
 		slog.Info("finish writing to stdin")
+		// TODO: Read from our inputchannel and write forever
 	}()
+	if err = c.Start(); err != nil {
+		return fmt.Errorf("c.Start() error: %w", err)
+	}
+	return nil
+}
 
-	err = c.Start()
+// TODO: Create a client that will send a specific QPS!
+// TODO: It would be cool to have a HTTP interface for results, then maybe the runs could be done on a K8s cluster?
+// TODO: I feel like it would be nicer to run docker containers, maybe way later.
+func main() {
+	start := time.Now()
+	store := SetupStore(start)
+	network := NewNetwork(store)
+
+	serverExecPath := "./echo-server"
+	err := network.StartNode("n1", serverExecPath)
+	if err != nil {
+		slog.Error("Start returned an error", "error", err)
+	} else {
+		slog.Info("Start returned without error")
+	}
+	err = network.StartNode("n2", serverExecPath)
 	if err != nil {
 		slog.Error("Start returned an error", "error", err)
 	} else {
@@ -77,6 +126,5 @@ func main() {
 	}
 
 	slog.Info("Waiting")
-	wg.Wait()
-	slog.Info("Wait finished")
+	<-make(chan struct{})
 }
